047d302cc1b7bb18ccc23817c3912f51cd308a85
jameshsu1125 <jameshsu1125@gmail.com>
2024-11-14 15:46:05 +0800

    新增 vite-plugin-compression 依賴，並更新 vite.config.ts 插件配置

--- apps/monopoly-go/vite.config.ts
+++ apps/monopoly-go/vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import { certificateFor } from 'devcert';
 import { resolve } from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
+// import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
@@ -35,7 +35,10 @@ export default defineConfig(async ({ mode }) => {
         },
       },
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      // viteCompression()
+    ],
     resolve: {
       alias: {
         '@': resolve(__dirname, 'src'),
